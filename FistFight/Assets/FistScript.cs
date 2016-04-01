d30ee8b47a05f6eb4d0a8c4790698e7b2f29f6f2
msik314 <msik314@gmail.com>
2016-04-01 19:15:54 -0400

    Added Fist Script

---
+++ FistFight/Assets/FistScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FistScript : MonoBehaviour {
+	[SerializeField] private string hAxis;
+	[SerializeField] private string vAxis;
+	[SerializeField] private float speed;
+	[SerializeField] private float recoil;
+	[SerializeField] private float lockTime;
+	private bool locked;
+	private Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		rbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		if (!locked)
+		{
+			Vector2 vel = new Vector2(speed * Input.GetAxisRaw(hAxis), speed * Input.GetAxisRaw(vAxis));
+			rbody.velocity = vel;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		StartCoroutine (bounce());
+		Vector2 normal = Vector2.zero;
+		int counter = 0;
+		foreach (ContactPoint2D c in col.contacts)
+		{
+			normal += c.normal;
+			++counter;
+		}
+		normal /= counter;
+		rbody.velocity = recoil * normal;
+	}
+
+	IEnumerator bounce()
+	{
+		locked = true;
+		yield return new WaitForSeconds (lockTime);
+		locked = false;
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # FistFight
-============
 Serious Game Jam Project
