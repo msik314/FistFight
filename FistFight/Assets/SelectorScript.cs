d8ccfa1f329c248bc78a07b8933f18361dfa709f
msik314 <msik314@gmail.com>
2016-04-02 01:10:15 -0400

    Added Menu Selector script

---
+++ FistFight/Assets/SelectorScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectorScript : MonoBehaviour {
+	[SerializeField] private Vector3 topPos;
+	[SerializeField] private Vector3 botPos;
+	[SerializeField] private string inputAxis;
+	[SerializeField] private string selectAxis;
+	private bool top;
+
+
+	// Use this for initialization
+	void Awake () {
+		transform.position = topPos;
+		top = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw (inputAxis) > 0.5) {
+			top = true;
+			transform.position = topPos;
+		}
+
+		if (Input.GetAxisRaw (inputAxis) < -0.5) {
+			top = false;
+			transform.position = botPos;
+		}
+
+		if (Input.GetAxisRaw (selectAxis) > 0.5) {
+			if (top) {
+				Application.LoadLevel (1);
+			} else {
+				Application.Quit ();
+			}
+		}
+	}
+}
