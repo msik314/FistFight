ac7235bdca4a67ec265b89cba97685993dcad9c4
msik314 <msik314@gmail.com>
2016-04-02 01:48:53 -0400

    MVP done

--- FistFight/Assets/FaceScript.cs
+++ FistFight/Assets/FaceScript.cs
@@ -3,23 +3,31 @@ using System.Collections;
 
 public class FaceScript : MonoBehaviour {
 	[SerializeField] private float FlinchTime;
+	[SerializeField] private Sprite normal;
+	[SerializeField] private Sprite flinch;
 	private int hits;
+	private SpriteRenderer sr;
 	// Use this for initialization
 	void Awake () {
 		hits = 0;
+		sr = GetComponent<SpriteRenderer> ();
+		sr.sprite = normal;
 	}
 
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		++hits;
+		StartCoroutine (hit ());
 		col.gameObject.SendMessage ("reset");
 	}
 
 
 	IEnumerator hit()
 	{
+		sr.sprite = flinch;
 		yield return new WaitForSeconds (FlinchTime);
+		sr.sprite = normal;
 	}
 
 	public int getHits()
